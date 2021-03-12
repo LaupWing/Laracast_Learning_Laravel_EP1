bb114b21ca15f3f3d598246d537b022ecaa5da7e
LaupWing <loc_strict@hotmail.com>
2021-03-12 13:38:23 +0100

    16

--- basics/routes/web.php
+++ basics/routes/web.php
@@ -14,10 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-   return [
-      'test'=> 'test'
-   ];
-   // return view('welcome');
+   return view('welcome');
 });
 
 Route::get('/about', function () {
